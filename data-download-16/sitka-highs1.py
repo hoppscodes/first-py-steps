787dd3b4bbbd46a5c1dd92f4884fff9e10e7d999
Trus96 <frosinas.of@gmail.com>
2023-11-15 13:28:29 +0100

    add sitka-highs1 file

---
+++ data-download-16/sitka-highs1.py
@@ -0,0 +1,12 @@
+#this file prints tmax values contained in row 5 of the file.
+import csv
+filename = 'C:/Users/samue/first-py-steps/first-py-steps/data-download-16/chapter_16/the_csv_file_format/data/sitka_weather_07-2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+print(highs)
