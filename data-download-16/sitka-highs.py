94be7a519c4ce275babc5387ac207b640df4b82a
Trus96 <frosinas.of@gmail.com>
2023-11-15 13:06:50 +0100

    add sitka-highs.py

--- data-download-16/sitka-highs.py
+++ data-download-16/sitka-highs.py
@@ -1,5 +1,5 @@
 import csv
-filename = 'C:\Users\samue\first-py-steps\first-py-steps\data-download-16\chapter_16\the_csv_file_format\data\sitka_weather_07_2018_simple.csv'
+filename = 'C:/Users/samue/first-py-steps/first-py-steps/data-download-16/chapter_16/the_csv_file_format/data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
