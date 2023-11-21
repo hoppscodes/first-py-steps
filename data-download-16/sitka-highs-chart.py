2b34b07e5ce5c00f578ff68d7c32b21cdb06b560
Trus96 <frosinas.of@gmail.com>
2023-11-22 00:01:27 +0100

    add dates to the chart file

--- data-download-16/sitka-highs-chart.py
+++ data-download-16/sitka-highs-chart.py
@@ -1,23 +1,27 @@
 import csv
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 filename = 'chapter_16/the_csv_file_format/data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
 #chart data   
 plt.style.use('seaborn-v0_8-dark')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 
 #chart format
 ax.set_title("highest temp recorded, july 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temp (F)", fontsize=16)
 ax.tick_params(axis='both', which='major', labelsize=16)
 
