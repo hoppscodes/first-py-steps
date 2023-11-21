4b3c5f44306a8e5d9a1806c727bd49007a056e96
Trus96 <frosinas.of@gmail.com>
2023-11-21 23:49:35 +0100

    patch change

--- data-download-16/sitka-highs1.py
+++ data-download-16/sitka-highs1.py
@@ -1,6 +1,6 @@
 #this file prints tmax values contained in row 5 of the file.
 import csv
-filename = 'C:/Users/samue/first-py-steps/first-py-steps/data-download-16/chapter_16/the_csv_file_format/data/sitka_weather_07-2018_simple.csv'
+filename = 'chapter_16/the_csv_file_format/data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
